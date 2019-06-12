bee606ab65e6483a0e62ef2a40f799613924a73f
chris13012 <christophecaillet@icloud.com>
2019-06-12 13:57:30 +0200

    changes midi

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,9 +1,40 @@
 import React, { Component } from "react";
 import Liste from "./liste";
-import MapContainer from "./mapContainer";
+import Map from "./map";
+import listeResto from "../listeResto.json";
 
 class Container extends Component {
-  state = {};
+  state = { restaurants: [], markers: [] };
+  componentDidMount() {
+    listeResto.forEach(resto => {
+      resto.placeId = 0;
+    });
+    this.setState({ restaurants: listeResto });
+  }
+  placerMarker = (resto, map) => {
+    let { markers } = this.state;
+    var imageResto = "icons/resto.png";
+    var markerResto = new window.google.maps.Marker({
+      position: new window.google.maps.LatLng(resto.lat, resto.long),
+      map: map,
+      icon: imageResto,
+      title: resto.restaurantName
+    });
+    markers.push(markerResto);
+    markerResto.addListener("click", function() {
+      map.setCenter(markerResto.getPosition());
+      /*
+          let indexMarker = markers.length - 1;
+      infoResto.avis(resto);
+      outils.bounce(markerResto);
+      outils.clignoterResto(indexMarker);
+      setTimeout(function() {
+        outils.scrollTo(indexMarker);
+      }, 1000);
+      */
+    });
+    this.setState({ markers });
+  };
   render() {
     return (
       <div
@@ -13,10 +44,14 @@ class Container extends Component {
           borderRadius: 5,
           display: "flex",
           flexDirection: "row",
-          padding: 5
+          padding: 5,
+          height: 750
         }}
       >
-        <MapContainer />
+        <Map
+          placerMarker={this.placerMarker}
+          restaurants={this.state.restaurants}
+        />
         <Liste />
       </div>
     );
