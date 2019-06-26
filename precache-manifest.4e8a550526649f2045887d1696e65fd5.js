115a35a54609df835efc724eb0663b894c1757c5
chris13012 <christophecaillet@icloud.com>
2019-06-26 10:15:11 +0200

    Updates

---
+++ precache-manifest.4e8a550526649f2045887d1696e65fd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb3bf766f9b01afa482a9f92f81bc82f",
+    "url": "/avosavisReact/index.html"
+  },
+  {
+    "revision": "dc68a9c4b4b3915db854",
+    "url": "/avosavisReact/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "34a0a82d8835f3ee4886",
+    "url": "/avosavisReact/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "dc68a9c4b4b3915db854",
+    "url": "/avosavisReact/static/js/2.2d99e22d.chunk.js"
+  },
+  {
+    "revision": "34a0a82d8835f3ee4886",
+    "url": "/avosavisReact/static/js/main.2a6d1021.chunk.js"
+  },
+  {
+    "revision": "4373b8e8102e7583aa55",
+    "url": "/avosavisReact/static/js/runtime~main.99a3f58a.js"
+  }
+]);
