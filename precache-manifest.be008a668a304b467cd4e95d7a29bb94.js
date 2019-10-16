a15a7b292da191836e701d6d6d396f67db59e5a0
chris13012 <christophecaillet@icloud.com>
2019-10-16 13:41:18 +0200

    Updates

--- precache-manifest.be008a668a304b467cd4e95d7a29bb94.js
+++ precache-manifest.be008a668a304b467cd4e95d7a29bb94.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bdbf63cd60c6579e596b6b85bb31ed8a",
+    "revision": "c296d86d69d34a2545864dbcbe501e72",
     "url": "/avosavisReact/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/avosavisReact/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "dcf0eb5026ac8d58e15d",
+    "revision": "31e0ad0fbfffbf4b8e6f",
     "url": "/avosavisReact/static/css/main.2cce8147.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/avosavisReact/static/js/2.a307946b.chunk.js"
   },
   {
-    "revision": "dcf0eb5026ac8d58e15d",
-    "url": "/avosavisReact/static/js/main.3c415187.chunk.js"
+    "revision": "31e0ad0fbfffbf4b8e6f",
+    "url": "/avosavisReact/static/js/main.2e812d7b.chunk.js"
   },
   {
     "revision": "4373b8e8102e7583aa55",
